9aad2a41a828c1495822144452e3315a8526a611
tts-doveloper2 <ttsjk.2020.02@gmail.com>
2020-06-27 17:22:38 +0800

    sidebar done with scroll percentage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Sidebar/>
+      <div className="mainContent">
+      <h2>Auto Sidebar</h2>
+  <p>This sidebar is as tall as its content (the links), and is always shown.</p>
+  <p>Scroll down the page to see the result.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>This sidebar is as tall as its content (the links), and is always shown.</p>
+  <p>Scroll down the page to see the result.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,66 @@
+import React, {useState, useEffect} from 'react';
+import './Sidebar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCoffee, faPlus  } from '@fortawesome/free-solid-svg-icons'
+
+const Sidebar = () => {
+    const [scrollPercent, setScrollPercent] = useState(0)
+
+    
+
+    useEffect(() => {
+        function onScroll() {
+            const result = ((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100)
+            const perfectResult = result>99.8 ? 100 : result.toFixed(2)
+            setScrollPercent(perfectResult)
+        }
+    
+        return window.addEventListener("scroll", onScroll)
+    }, [scrollPercent])
+
+    return (
+        <div class="sidenav sidebar">
+            <button className="socialIcons">
+                <a href="#">
+                    {/* <FontAwesomeIcon icon={faCoffee} /> */}
+                    <i class="fa fa-facebook" aria-hidden="true"></i>
+                </a>
+            </button>
+            <br/>
+            <button className="socialIcons">
+                <a href="#">
+                    <i class="fa fa-twitter" aria-hidden="true"></i>
+                </a>
+            </button>
+            <br/>
+            <button className="socialIcons">
+                <a href="#">
+                    <i class="fa fa-google-plus" aria-hidden="true"></i>
+                </a>
+            </button>
+            <br/>
+            <button className="socialIcons">
+                <a href="#">
+                    <i class="fa fa-pinterest-p" aria-hidden="true"></i>
+                </a>
+            </button>
+            <br/>
+            <button className="socialIcons">
+                <a href="#">
+                    <i class="fa fa-linkedin" aria-hidden="true"></i>
+                </a>
+            </button>
+            <hr style={{width: '50px'}}/>
+            <button className="socialIcons plusIcon">
+                <a href="#"><FontAwesomeIcon icon={faPlus} /></a>
+            </button>
+            
+            <button className="percentageIcon">
+                <p>{scrollPercent}%</p>
+            </button>
+        </div>
+    );
+}
+
+export default Sidebar;
