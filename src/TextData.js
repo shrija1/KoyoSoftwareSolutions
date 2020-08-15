43df523d68d7dc6c0435d4de25c99d7c99371729
Bhavyam Goyal <bhavyam.kr.goyal@gmail.com>
2020-08-15 19:44:10 +0530

    added text data file

---
+++ src/TextData.js
@@ -0,0 +1,74 @@
+var WebsiteText=null;
+if(WebsiteText===null){
+    WebsiteText={
+        home:{
+            heading:"this is the home heading",
+            headerText:"We are team of talanted digital marketers",
+            headerCardsData: [
+                {
+                    classBox: '',
+                    icon: 'ri-store-line',
+                    name: 'Lorem Ipsum'
+                },
+                {
+                    classBox: '',
+                    icon: 'ri-bar-chart-box-line',
+                    name: 'Dolor Sitema'
+                },
+                {
+                    classBox: 'mt-4 mt-md-0',
+                    icon: 'ri-calendar-todo-line',
+                    name: 'Sedare Perspiciatis'
+                },
+                {
+                    classBox: 'mt-4 mt-xl-0',
+                    icon: 'ri-paint-brush-line',
+                    name: 'Magni Dolores'
+                },
+                {
+                    classBox: 'mt-4 mt-xl-0',
+                    icon: 'ri-database-2-line',
+                    name: 'Nemos Enimade'
+                }
+            ],
+            introductionHeading:"Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.",
+            introductionText:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et doloremagna aliqua.",
+            introductionCheckList1:"Ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            introductionCheckList2:"Duis aute irure dolor in reprehenderit in voluptate velit.",
+            introductionCheckList3:"Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.",
+            introductionEndingText:"Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident",
+            featuresListData:[
+                {
+                    classDiv: 'mt-lg-0',
+                    icon: 'bx-receipt',
+                    title: 'Est labore ad',
+                    description: 'Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip'
+                },
+                {
+                    classDiv: '',
+                    icon: 'bx-cube-alt',
+                    title: 'Harum esse qui',
+                    description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt'
+                },
+                {
+                    classDiv: '',
+                    icon: 'bx-images',
+                    title: 'Aut occaecati',
+                    description: 'Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere'
+                },
+                {
+                    classDiv: '',
+                    icon: 'bx-shield',
+                    title: 'Beatae veritatis',
+                    description: 'Expedita veritatis consequuntur nihil tempore laudantium vitae denat pacta'
+                },
+            ]
+        },
+        aboutUsHeading:{
+            heading:"",
+            headerText:"",
+        }
+    }
+}
+
+export default WebsiteText
--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -1,62 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import './Header.css'
 import HeaderElement from './HeaderElement'
+import WebText from '../../../TextData';
 
 function Header() {
     // fetch('https://api.proarif.com/softsite/Home/HeaderElement.json')
     // .then(response => response.json())
     // .then(json => console.log(json))
 
-
-    
-
-
-    // console.log(data1)
-
-    const data = [
-        {
-            classBox: '',
-            icon: 'ri-store-line',
-            name: 'Lorem Ipsum'
-        },
-        {
-            classBox: '',
-            icon: 'ri-bar-chart-box-line',
-            name: 'Dolor Sitema'
-        },
-        {
-            classBox: 'mt-4 mt-md-0',
-            icon: 'ri-calendar-todo-line',
-            name: 'Sedare Perspiciatis'
-        },
-        {
-            classBox: 'mt-4 mt-xl-0',
-            icon: 'ri-paint-brush-line',
-            name: 'Magni Dolores'
-        },
-        {
-            classBox: 'mt-4 mt-xl-0',
-            icon: 'ri-database-2-line',
-            name: 'Nemos Enimade'
-        }
-    ]
     return (
         <section id="hero" className="d-flex align-items-center justify-content-center">
             <div className="container" data-aos="fade-up">
 
             <div className="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
                 <div className="col-xl-6 col-lg-8">
-                <h1>Powerful Digital Solutions With SoftSite<span>.</span></h1>
-                <h2>We are team of talanted digital marketers</h2>
+                <h1>{WebText.home.heading}<span>.</span></h1>
+                <h2>{WebText.home.headerText}</h2>
                 </div>
             </div>
 
             <div className="row mt-5 justify-content-center" data-aos="zoom-in" data-aos-delay="250">
-                
                 {
-                    data.map(element => <HeaderElement key={element.name} data={element}/>)
+                    WebText.home.headerCardsData.map(element => <HeaderElement key={element.name} data={element}/>)
                 }
-                
             </div>
 
             </div>
