8d933e6bc2ca5e4c0922033f0d24948667bfda74
shrija1 <67973633+shrija1@users.noreply.github.com>
2021-07-26 16:48:26 +0530

    Update TeamMember.js

--- src/components/Home/Team/TeamMember.js
+++ src/components/Home/Team/TeamMember.js
@@ -1,48 +1,52 @@
-import React from 'react';
-import './Team.css';
+import React from "react";
+import "./Team.css";
 
 const Team = (props) => {
-    const { data_aos_delay, img, name, title, description } = props.member
+  const { data_aos_delay, img, name, title, description } = props.member;
 
-    return (
+  return (
+    <div className="col-lg-4 col-md-6 mt-4 d-flex align-items-stretch teamm">
+      <div className="member-img">
+        <img
+          src={img}
+          style={{ width: "100%" }}
+          className="img-fluid teamim"
+          alt=""
+        />
 
+        <div
+          className="member"
+          data-aos="fade-up"
+          data-aos-delay={data_aos_delay}
+        >
+          <div className="social">
+            {/* <a href="/#"><i className="icofont-twitter"></i></a> */}
+            {/* <a href="/#"><i className="icofont-facebook"></i></a> */}
 
-
-
-
-        <div className="col-lg-4 col-md-6 mt-4 d-flex align-items-stretch teamm">
-
-            <div className="member-img">
-                <img src={img} style={{ width: '100%' }} className="img-fluid teamim" alt="" />
-
-
-                <div className="member" data-aos="fade-up" data-aos-delay={data_aos_delay}>
-                    <div className="social">
-                        {/* <a href="/#"><i className="icofont-twitter"></i></a> */}
-                        {/* <a href="/#"><i className="icofont-facebook"></i></a> */}
-
-                        <a href="/#"><i className="icofont-linkedin"></i></a>
-                    </div>
-                </div>
-
-                <div className="card" >
-                    <div className="card-body border-top-0">
-                        <h5 className="card-title border-top-0">{name}</h5>
-                        <h6 className="card-subtitle mb-2 ">{title}</h6>
-                        <div className="cardsocial">
-                            <a href="/#" class="card-link"><i className="icofont-envelope"></i></a>
-                            <a href="/#" class="card-link"><i className="icofont-linkedin"></i></a>
-                        </div>
-                        <p className="card-text">{description}
-                        </p>
-
-                    </div>
-                </div>
-
-            </div>
+            <a href="/#">
+              <i className="icofont-linkedin"></i>
+            </a>
+          </div>
         </div>
 
-    )
-}
+        <div className="card">
+          <div className="card-body border-top-0">
+            <h5 className="card-title border-top-0">{name}</h5>
+            <h6 className="card-subtitle mb-2 ">{title}</h6>
+            <div className="cardsocial">
+              <a href="/#" class="card-link">
+                <i className="icofont-envelope"></i>
+              </a>
+              <a href="/#" class="card-link">
+                <i className="icofont-linkedin"></i>
+              </a>
+            </div>
+            <p className="card-text">{description}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Team;
